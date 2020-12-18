5a08471dc351b778ce49a2261e9a614dfd431fec
Clayton Sexton <csbeasle@uark.edu>
2020-12-18 15:33:24 -0600

    Use correct link to download zip file of searches

--- src/frontend/components/profile-components/Searches.vue
+++ src/frontend/components/profile-components/Searches.vue
@@ -30,7 +30,7 @@
               </template>
               <template v-slot:cell(actions)="row" class="actions">
                 <b-button variant="primary" v-text="'Request Download'" @click="requestDownload(row.item)" :disabled="row.item.process || (row.item.path !== '' && row.item.path !== null)"></b-button>
-                <b-button variant="primary" target="_blank" v-text="'Download'" :disabled="!row.item.path" :href="`${$apiUrl}${row.item.path}`"></b-button>
+                <b-button variant="primary" target="_blank" v-text="'Download'" :disabled="!row.item.path" :href="link(row.item.path)"></b-button>
               </template>
             </b-table>
           </b-card>
@@ -110,6 +110,9 @@ export default {
     }
   },
   methods: {
+    link(val) {
+      return `${this.$apiUrl}/${_.trimStart(val, '/public')}`
+    },
     getSearches() {
       this.$apollo.queries.searchesQuery.refetch()
     },
